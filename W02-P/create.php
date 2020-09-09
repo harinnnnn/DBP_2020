6c52a1d93deafdf3a85a5d6844539c2c6511137e
soihs821 <53467524+soihs821@users.noreply.github.com>
2020-09-09 22:25:25 +0900

    Update create.php

--- W02-P/create.php
+++ W02-P/create.php
@@ -1,1 +1,43 @@
+<?php
+  $link = mysqli_connect('localhost:3307', 'root', 'root1234', 'dbp');
+  $query = "SELECT * FROM w02p";
+  $result = mysqli_query($link, $query);
+  $list = ' ';
+  while ($row = mysqli_fetch_array($result)) {
+    $list = $list."<li>
+    <a href=\"index.php?id={$row['id']}\">{$row['title']}</li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'description' => 'Database is ...'
+  );
+
+  if (isset($_GET['id'])) {
+  $query = "SELECT * FROM w02p WHERE id={$_GET['id']}";
+  $result = mysqli_query($link, $query);
+  $row = mysqli_fetch_array($result);
+  $article = array(
+    'title' => $row['title'],
+    'description' => $row['description']
+  );
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> ALPHAFRUIT </title>
+</head>
+<body>
+  <h1><a href="index.php"> ALPHAFRUIT </h1>
+  <ol> <?= $list ?> </ol>
+  <form action="process_create.php" method="post">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p><textarea name="description" placeholder="description"></textarea></p>
+    <p><input type="submit"</p>
+  </form>
+</body>
+</html>
 
